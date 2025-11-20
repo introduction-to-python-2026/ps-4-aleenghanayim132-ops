6e9c6ccb36631591c9196da5a13b73bc78e4f2ea
aleenghanayim132-ops <aleenghanayim132@gmail.com>
2025-11-20 13:56:14 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,19 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    start = 0
+    list_of_parts = []
+    for i in range(len(formula)):
+        if formula[i].isupper():
+            list_of_parts.append(formula[start:i])
+            start = i
+    list_of_parts.append(formula[start:])
+    return list_of_parts
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    digit_location = 1
+    for char in formula[1:]:
+        if char.isdigit():
+            break
+        digit_location += 1
+    if digit_location == len(formula):
+        return formula, 1
